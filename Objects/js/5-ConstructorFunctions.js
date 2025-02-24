d47d36c6e24f665764015cc8671ab377ac7231b3
alfee77 <103679854+alfee77@users.noreply.github.com>
2025-02-24 20:57:21 +0000

    Finished the writeup on constructors

--- Objects/js/5-ConstructorFunctions.js
+++ Objects/js/5-ConstructorFunctions.js
@@ -1,5 +1,5 @@
-function example1(){
-    function User(name){
+function example1() {
+    function User(name) {
         this.name = name;
         this.isAdmin = false;
     }
@@ -8,4 +8,27 @@ function example1(){
     let user2 = new User("Fiona");
 
     alert(`${user1.name} says hi! ${user2.name} says howdy!`);
+}
+
+function example2() {
+    function User(name="Alistair", age=32, occupation="JSMonkey") {
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+
+        this.sayHi = function () {
+            alert(`My name is: ${this.name}, and I'm ${this.age} years old. I fill my day working as a(n) ${this.occupation}`);
+        };
+    }
+
+    let john = new User("John", 47, "Electrical Engineer");
+
+    john.sayHi(); // My name is: John</span>
+        
+    /*
+    john = {
+        name: "John",
+        sayHi: function() { ... }
+    }
+    */
 }
