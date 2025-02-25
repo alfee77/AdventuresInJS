a79322fdf6bbb1591665e18ccc53acdfe3a0a98f
alfee77 <103679854+alfee77@users.noreply.github.com>
2025-02-25 09:17:38 +0000

    Started task 2 in object functions

--- Objects/js/5-ConstructorFunctions.js
+++ Objects/js/5-ConstructorFunctions.js
@@ -32,3 +32,15 @@ function example2() {
     }
     */
 }
+
+function task1(){
+
+    let obj = {};
+    function A() { return obj }
+    function B() { return obj }
+
+    let a = new A();
+    let b = new B();
+
+    alert( a == b ); // true
+}
