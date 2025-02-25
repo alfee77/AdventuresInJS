159b98de1bb2c66bcaa17d0a0059eb94e91d7cc2
alfee77 <103679854+alfee77@users.noreply.github.com>
2025-02-25 21:07:11 +0000

    Hunners really

--- Objects/js/5-ConstructorFunctions.js
+++ Objects/js/5-ConstructorFunctions.js
@@ -67,3 +67,22 @@ function task2(){
         }
     }
 }
+
+function task3(){
+    let accumulator = new Accumulator(1); // initial value 1
+
+    accumulator.read(); // adds the user-entered value
+    accumulator.read(); // adds the user-entered value
+
+    alert(accumulator.value); // shows the sum of these values
+
+    function Accumulator(startingValue){
+        
+        this.value = +startingValue;
+
+        this.read = function(){
+            this.value += +prompt("How much to add? ", 0);
+        };
+    }
+
+}
