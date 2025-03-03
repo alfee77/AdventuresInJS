4ad6c97f2b262ef8b263d6ebd3c7924250e971ff
alfee77 <103679854+alfee77@users.noreply.github.com>
2025-03-03 18:42:46 +0000

    Right - I'm not doing no more on Symbols!!!

--- Objects/js/7-Symbol type.js
+++ Objects/js/7-Symbol type.js
@@ -4,3 +4,24 @@ function example1(){
     
     alert(id1 === id2); // false
 }
+
+function example2(){
+    let id = Symbol("id");
+    let user = {
+        name: "John",
+        age: 30,
+        [id]: 123
+    };
+    for (let key in user) alert(key); // name, age (no symbols)
+    // the direct access by the symbol works
+    alert( "Direct: " + user[id] ); // Direct: 123
+}
+
+function example3(){
+    let id = Symbol("id");
+    let user = {
+        [id]: 123
+    };
+    let clone = Object.assign({}, user);
+    alert( clone[id] ); // 123
+}
