dbc7270fc8efdb7bbe77d92e734ad0888bbaf9a0
alfee77 <103679854+alfee77@users.noreply.github.com>
2025-03-21 08:02:52 +0000

    lorem

--- Fundamentals/js/2-codeStructure.js
+++ Fundamentals/js/2-codeStructure.js
@@ -1,4 +1,5 @@
-function example2(){
+function example2() {
     alert("I'm JavaScript");
+    alert(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quaerat nostrum, quis eos, ex error, repellendus repellat quasi blanditiis quidem sapiente? Consequuntur quasi, iusto nemo maiores odio unde ad nobis!`);
     // document.getElementById("demo").innerHTML = "I'm JavaScript";
 }
